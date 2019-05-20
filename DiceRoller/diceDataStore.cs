7ed28764e5f4fcdb071c08bec13a02bd4462a000
liam-ash <liam.ash@xero.com>
2019-05-20 11:24:53 +1000

    first submission

---
+++ DiceRoller/diceDataStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiceRoll
+{
+    class DiceDataStore
+    {
+        private int diceSides;
+        private int diceNumber;
+        private string diceInput = "1d8";
+    }
+}
